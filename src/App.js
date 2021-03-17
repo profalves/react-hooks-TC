69339395f96290b4d6a7b52983cc82da412d5eb3
Rodrigo Alves <rodrigo.alves@acct.global>
2021-03-17 10:57:57 -0300

    step4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import TodoProvider from './context/TodoContext';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 function App() {
-  const [list, setList] = useState([
-    { id: 1, title: "Ir pra academia", done: true },
-    { id: 2, title: "Ir ao Supermercado", done: false },
-    { id: 3, title: "Estudar React", done: false }
-  ])
 
   const [total, setTotal] = useState(0)
 
   const handleInputChanged = (id) => {
-    const newList = list.map(todo => {
-      return todo.id === id ? { ...todo, done: !todo.done } : todo;
-    })
 
-    setList(newList)
   }
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!e.target[0].value) return;
-    setList([...list, {
-      id: list.length + 1,
-      title: e.target[0].value,
-      done: false
-    }])
-    e.target[0].value = '';
+
   }
 
   useEffect(() => {
@@ -35,23 +24,10 @@ function App() {
   }, [list])
 
   return (
-    <div className="App">
-      <h1>Lista de Tarefas</h1>
-      <strong>Tarefas concluidas: {total}</strong>
-      <ul className="todosList">
-        {list.map(todo =>
-          <li key={todo.id}>
-            {todo.title}
-            <input type="checkbox" onChange={() => handleInputChanged(todo.id)} checked={todo.done} />
-          </li>)
-        }
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Digite a nova tarefa" />
-        <br />
-        <button type="submit">Adicionar</button>
-      </form>
-    </div>
+    <TodoProvider>
+      <TodoList />
+      <AddTodo />
+    </TodoProvider>
   );
 }
 
